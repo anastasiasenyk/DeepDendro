d3ced124f689871e0e99954b3c118f0da4f279a1
yakorch <korchyar19@gmail.com>
2023-04-05 02:03:23 +0300

    error in dimension main

--- main.cpp
+++ main.cpp
@@ -7,10 +7,8 @@
 int main() {
     srand((unsigned int) time(0));
 
-    MatrixXd labels = MatrixXd::Random(1, 4);
+    MatrixXd labels = MatrixXd::Random(1, 400);
     labels = (labels.array() > 0).cast<double>();
-//    std::cout << labels << std::endl;
-
 
     Model model;
     model.addInput(MatrixXd::Random(784, 400));
@@ -18,7 +16,7 @@ int main() {
 
     model.addLayer(16, activation::relu);
     model.addLayer(8, activation::relu);
-    model.train(5000, 0.005);
+    model.train(10000, 0.005);
 
     return 0;
 }
