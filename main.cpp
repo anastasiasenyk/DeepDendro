3580db429d04722f1b8770327a213539100fa92a
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2023-04-05 18:27:05 +0300

    MNIST remove

--- main.cpp
+++ main.cpp
@@ -5,20 +5,38 @@
 #include "Model.h"
 #include "MNISTProcess.h"
 
+
 int main() {
-    int numTrainImg = 60000;
     srand((unsigned int) time(0));
+
+    int numImg = 60000;
+    int numTrainImg = std::floor(numImg * 0.95);
+    int numTestImg = numImg - numTrainImg;
+
 
     MNISTProcess mnistProcess = MNISTProcess();
     mnistProcess.skipHeaders("../MNIST_ORG/train-images.idx3-ubyte", "../MNIST_ORG/train-labels.idx1-ubyte", 16, 8);
-    MatrixXd images(784, numTrainImg);
-    MatrixXd labels(10, numTrainImg);
+
+    MatrixXd trainImages(784, numTrainImg);
+    MatrixXd trainLabels(10, numTrainImg);
+    MatrixXd testImages(784, numTestImg);
+    MatrixXd testLabels(10, numTestImg);
+
+
     for (int sample = 0; sample < numTrainImg; ++sample) {
         VectorXd oneHotLabel = mnistProcess.readLbl();
         VectorXd flattenedImage = mnistProcess.readImg(28, 28);
-        images.col(sample) = flattenedImage;
-        labels.col(sample) = oneHotLabel;
+        trainImages.col(sample) = flattenedImage;
+        trainLabels.col(sample) = oneHotLabel;
     }
+
+    for (int sample = 0; sample < numTestImg; ++sample) {
+        VectorXd oneHotLabel = mnistProcess.readLbl();
+        VectorXd flattenedImage = mnistProcess.readImg(28, 28);
+        testImages.col(sample) = flattenedImage;
+        testLabels.col(sample) = oneHotLabel;
+    }
+
 #ifdef DEBUG
     for(int k=0;k<10;++k) {
         for (int i = 0; i < 10; ++i) {
@@ -36,12 +54,12 @@ int main() {
 
 
     Model model;
-    model.addInput(images);
-    model.addOutput(labels);
+    model.addInput(trainImages);
+    model.addOutput(trainLabels);
 
     model.addLayer(16, activation::relu);
-    model.addLayer(8, activation::relu);
-    model.train(512, 0.001);
+    model.addLayer(16, activation::relu);
+    model.train(10000, 0.005);
 
     return 0;
 }
