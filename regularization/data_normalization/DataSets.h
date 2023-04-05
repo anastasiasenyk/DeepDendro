b1d74dbb6ffc22765653f72bf1be3f2dda960c59
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2023-04-05 22:52:01 +0300

    minor (Datasets)

---
+++ regularization/data_normalization/DataSets.h
@@ -0,0 +1,17 @@
+//
+// Created by Anastasiaa on 05.04.2023.
+//
+
+#ifndef DEEPDENDRO_DATASETS_H
+#define DEEPDENDRO_DATASETS_H
+
+#endif //DEEPDENDRO_DATASETS_H
+
+#include "Layer.h";
+
+struct DataSets {
+    MatrixXd testData;
+    MatrixXd testLabels;
+    MatrixXd trainData;
+    MatrixXd trainLabels;
+} ;
